62c43fd64bbc0292b6b62beba4cddc511138b0ab
Ross Jones <rossmosis.jones@gmail.com>
2019-04-17 14:20:53 -0400

    refactored action cable

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
+    @buddy = Buddy.find(params[:message][:buddy_id])
+    reciever_id = @buddy.requester_id == @message.user_id ? @buddy.requestee_id : @buddy.requester_id
     if @message.save
-      message_cable(@message)
+      message_cable(@message, reciever_id)
+      message_user_cable(@message)
       render json: {
         success: true, object: @message, response_status: '200'
       }
@@ -17,9 +20,21 @@ class Api::V1::MessagesController < ApplicationController
     params.require(:message).permit(:user_id, :buddy_id, :content, :read)
   end
 
-  def message_cable(message)
+  def message_cable(message, reciever_id)
     ActionCable.server.broadcast(
-      "message_channel_#{message.buddy_id}",
+      "message_channel_#{reciever_id}",
+      content: message.content,
+      id: message.id,
+      username: message.user_name,
+      buddy_id: message.buddy_id,
+      read: message.read,
+      created_at: message.created_at
+    )
+  end
+
+  def message_user_cable(message)
+    ActionCable.server.broadcast(
+      "message_channel_#{message.user_id}",
       content: message.content,
       id: message.id,
       username: message.user_name,
