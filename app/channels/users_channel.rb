d9884f0228047faf5e2103720106551d90c43fd2
Ross Jones <rossmosis.jones@gmail.com>
2019-06-02 18:06:44 -0400

    removing unused channels

--- app/channels/users_channel.rb
+++
@@ -1,9 +0,0 @@
-class UsersChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "users_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
